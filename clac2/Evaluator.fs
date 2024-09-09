97f211b296ce28d53cec6e387da92d7939fa70dd
Davide R. Wiest <davide.wiest2@gmail.com>
2024-09-09 08:41:38 +0200

    Update Evaluator.fs

--- clac2/Evaluator.fs
+++ clac2/Evaluator.fs
@@ -4,24 +4,33 @@ open Clac2.Domain
 open Clac2.Utilities
 open FSharp.Core.Result
 
-let evaluate (stdCtx: StandardContext) (lines: Line array) : Result<DefinedValue, string> array  =
-    let customAssignments = lines |> Array.choose (fun x -> match x with | Assignment a -> Some a | _ -> None)
-    let manipulations = lines |> Array.choose (fun x -> match x with | Expression e -> Some e | _ -> None)
+type EvalCtx = {
+    customAssignmentMap: Map<string, CallableFunction>
+    stdFunctionsMap: Map<string, DefinedCallableFunction>
+}
 
-    let assignmentMap = customAssignments |> Array.map (fun x -> x.name, x) |> Map.ofArray
-    let stdFunctionsMap = stdCtx.definedCtx.functions |> Array.map (fun x -> x.name, x) |> Map.ofArray
+let evaluateAll (stdCtx: StandardContext) (lines: Line array) : Result<DefinedValue, string> array  =
+    let customAssignmentMap = 
+        lines 
+        |> Array.choose (fun x -> match x with | Assignment a -> Some a | _ -> None)
+        |> Array.map (fun x -> x.name, x) |> Map.ofArray
+        
+    let evalCtx = {
+        customAssignmentMap: customAssignmentMap
+        stdFunctionsMap: stdCtx.definedCtx.functions |> Array.map (fun x -> x.name, x) |> Map.ofArray
+    }
 
-    manipulations
-    |> Array.map (evaluateManipulation stdFunctionsMap assignmentMap)
+    lines 
+    |> Array.choose (fun x -> match x with | Expression e -> Some e | _ -> None)
+    |> Array.map (evaluateOne evalCtx)
 
-let evaluateManipulation stdFunctionsMap assignmentMap manipulation : Result<DefinedValue, string> array  =
-
+let evaluateOne evalCtx manipulation : Result<DefinedValue, string> array  =
     manipulation
     |> fun m -> m[0], substituteMany stdFunctionsMap assignmentMap Map.empty m[1..]
     |> fun (startFn, args) -> args |> bind (eval stdFunctionsMap assignmentMap startFn)
     |> combineResultsToArray
 
-let rec substituteOne stdFunctionsMap (assignmentMap: Map<string, CallableFunction>) (substitutions: Map<string, DefinedValue>) x =
+let rec substituteOne evalCtx (assignmentMap: Map<string, CallableFunction>) (substitutions: Map<string, DefinedValue>) x =
     match x with
     | Primitive p -> p |> DefinedPrimitive |> Ok
     | Fn f -> 
@@ -31,28 +40,21 @@ let rec substituteOne stdFunctionsMap (assignmentMap: Map<string, CallableFuncti
         else
         if stdFunctionsMap.ContainsKey f then toDefinedFn stdFunctionsMap f |> Ok else failwith ("Function not found: " + f)
 
-let substituteMany stdFunctionsMap assignmentMap (substitutions: Map<string, DefinedValue>) m : Result<DefinedValue array, string> = 
-    m 
-    |> Array.map (substituteOne stdFunctionsMap assignmentMap substitutions)
-    |> combineResultsToArray
+let substituteMany evalCtx (substitutions: Map<string, DefinedValue>) m : Result<DefinedValue array, string> = 
+    m |> Array.map (substituteOne evalCtx substitutions) |> combineResultsToArray
 
-let rec eval stdFunctionsMap assignmentMap (startFn: Reference) (args: DefinedValue array) : Result<DefinedValue, string> =
+let rec eval evalCtx (startFn: Reference) (args: DefinedValue array) =
     let buildArgs (argsBefore: DefinedValue array) (signature: FnType array) =
         if argsBefore.Length = signature.Length - 1 then argsBefore |> Ok else 
         
         let args' = argsBefore[0..signature.Length - 3]
-        let lastArg = eval stdFunctionsMap assignmentMap (definedValueToReference argsBefore[signature.Length - 2]) argsBefore[signature.Length - 1..]
-
-        match lastArg with
-        | Error e -> Error e
-        | Ok lastArg -> args' |> Array.append [| lastArg |] |> Ok
+        evalOne evalCtx (definedValueToReference argsBefore[signature.Length - 2]) argsBefore[signature.Length - 1..]
+        |> bind(fun lastArg -> args' |> Array.append [| lastArg |] |> Ok)
 
     match startFn with
     | Primitive p -> p |> DefinedPrimitive |> Ok
     | Fn f -> 
-        printfn "Evaluating %s" f
-
-        let signature = if stdFunctionsMap.ContainsKey f then stdFunctionsMap[f].signature else assignmentMap[f].signature
+        let signature = if evalCtx.stdFunctionsMap.ContainsKey f then evalCtx.stdFunctionsMap[f].signature else evalCtx.assignmentMap[f].signature
 
         match buildArgs args signature with 
         | Error e -> Error e  
@@ -63,13 +65,10 @@ let rec eval stdFunctionsMap assignmentMap (startFn: Reference) (args: DefinedVa
             let fn = assignmentMap[f]
             let substitutions = Map.ofArray (args |> Array.zip fn.args)
 
-            // this shouldnt be necessary
-            if fn.fn.Length = 1 then substituteOne stdFunctionsMap assignmentMap substitutions fn.fn[0] else
-
             (substituteMany stdFunctionsMap assignmentMap substitutions fn.fn[1..])
             |> bind (fun x -> eval stdFunctionsMap assignmentMap fn.fn[0] x)
 
-let toDefinedFn stdFunctionsMap f = DefinedFn (stdFunctionsMap[f].name, stdFunctionsMap[f].DefinedFn)
+let toDefinedFn evalCtx f = DefinedFn (evalCtx.stdFunctionsMap[f].name, evalCtx.stdFunctionsMap[f].DefinedFn)
 
 let definedValueToReference (v: DefinedValue) =
     match v with
